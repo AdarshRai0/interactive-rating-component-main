88142ed90643cdfe4d0c2e21603795245f92dbe5
AdarshRai0 <adarshrai3011@gmail.com>
2022-08-11 09:59:45 +0530

    Interactive rating component

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const form = document.querySelector(".card__form");
+
+const rating_state = document.querySelector(".rating-state");
+const thanks_you_state = document.querySelector(".thanks-you-state");
+
+const selected = document.querySelector(".display-selected");
+
+form.addEventListener("submit", event => {
+
+    event.preventDefault();
+
+    const selected_value = form.elements.rating.value;
+
+    if(!selected_value) return;
+
+    selected.innerHTML = selected_value;
+
+    rating_state.style.display = "none";
+    thanks_you_state.style.display = "flex";
+
+})
